86c3706aaa1a2225147f1f36d8bd74f5513dea2d
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-05-22 07:08:33 -0500

    beautified files

--- src/ques1-4.js
+++ src/ques1-4.js
@@ -1,29 +1,29 @@
 //1
 const positiveSum = (numbers) => {
-    let sum = 0;
-      
-      for(const number of numbers) {
-          if(number > 0){
-              sum += number;
-          }
-      }
-      
-    return sum;
+  let sum = 0;
+
+  for (const number of numbers) {
+    if (number > 0) {
+      sum += number;
+    }
   }
 
-  //2
-  const isDivisible = (number, divisorA, divisorB) => number % divisorB === 0 && number % divisorA === 0
+  return sum;
+}
 
-  //3
-  const solution = (str) => str.split('').reverse().join('')
+//2
+const isDivisible = (number, divisorA, divisorB) => number % divisorB === 0 && number % divisorA === 0
 
-  //4
-  const reverseSeq = (n) => {
-    let answer = [];          //intialize an array
-    
-    for (let i=n; i>0; i--){  //loop down from n to 1
-      answer.push(i);         //push each i to the answer array
-    }
-    
-    return answer;            //return answer;
-  };
+//3
+const solution = (str) => str.split('').reverse().join('')
+
+//4
+const reverseSeq = (n) => {
+  let answer = []; //intialize an array
+
+  for (let i = n; i > 0; i--) { //loop down from n to 1
+    answer.push(i); //push each i to the answer array
+  }
+
+  return answer; //return answer;
+};
